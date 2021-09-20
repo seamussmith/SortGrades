be7293f0aeb72840ee5450b689064133d807bbbc
seamussmith <student.141504@worcesterschools.net>
2021-09-20 07:35:32 -0400

    Student class

--- src/main/App.java
+++ src/main/App.java
@@ -1,5 +1,22 @@
 package main;
 
+class Student
+{
+    public String firstName;
+    public String lastName;
+    public int studentId;
+    public double gpa;
+    public String grade;
+    public Student(String firstName, String lastName, int studentId, double gpa, String grade)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentId = studentId;
+        this.gpa = gpa;
+        this.grade = grade;
+    }
+}
+
 public class App
 {
     public static void main(String[] args) 
